3cda285e1d1b5766f040132bd4413021e6bfbf76
Dave Davis <dabukun@gmail.com>
2021-06-01 20:30:51 -0400

    Add local redirects for all personal profile links

--- next.config.js
+++ next.config.js
@@ -7,6 +7,26 @@ module.exports = {
           "https://docs.google.com/document/d/12g50qJem_twcVmCw90jw04F4wuytgCLcJpCELamek7g/edit?usp=sharing",
         permanent: true,
       },
+      {
+        source: "/github",
+        destination: "http://github.com/davertron",
+        permanent: true,
+      },
+      {
+        source: "/linkedin",
+        destination: "http://lnkd.in/ZNAJtW",
+        permanent: true,
+      },
+      {
+        source: "/hn",
+        destination: "http://news.ycombinator.com/user?id=Davertron",
+        permanent: true,
+      },
+      {
+        source: "/twitter",
+        destination: "https://twitter.com/davertron",
+        permanent: true,
+      },
     ];
   },
 };
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import GoogleAnalytics from '../components/GoogleAnalytics';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import GoogleAnalytics from "../components/GoogleAnalytics";
 
 export default function Home() {
   return (
@@ -9,22 +9,45 @@ export default function Home() {
         <title>davertron.com</title>
         <meta name="description" content="Personal Homepage for Dave Davis" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
 
         <GoogleAnalytics />
       </Head>
 
-        <div className={styles.container}>
-            <h1>Dave Davis</h1>
-            <p>Front-End Developer. JavaScript Jockey. React Wrangler. Dork Dad.</p>
-            <ul className={styles.links}>
-                <li><a target="_blank" href="https://docs.google.com/document/d/12g50qJem_twcVmCw90jw04F4wuytgCLcJpCELamek7g/edit?usp=sharing">Resume</a></li>
-                <li><a target="_blank" href="http://github.com/davertron">Github</a></li>
-                <li><a target="_blank" href="http://lnkd.in/ZNAJtW">LinkedIn</a></li>
-                <li><a target="_blank" href="http://news.ycombinator.com/user?id=Davertron">Hacker News</a></li>
-                <li><a target="_blank" href="https://twitter.com/davertron">Twitter</a></li>
-            </ul>
-        </div>
+      <div className={styles.container}>
+        <h1>Dave Davis</h1>
+        <p>Front-End Developer. JavaScript Jockey. React Wrangler. Dork Dad.</p>
+        <ul className={styles.links}>
+          <li>
+            <a target="_blank" href="/resume">
+              Resume
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="/github">
+              Github
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="/linkedin">
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="/hn">
+              Hacker News
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="/twitter">
+              Twitter
+            </a>
+          </li>
+        </ul>
+      </div>
     </>
-  )
+  );
 }
