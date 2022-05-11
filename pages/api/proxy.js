5887b132cad683f47a7b202ddbab111e8461f900
Dave Davis <dabukun@gmail.com>
2022-05-11 10:25:16 -0400

    Add proxy api endpoint

---
+++ pages/api/proxy.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import Cors from "cors";
+
+function initMiddleware(middleware) {
+  return (req, res) =>
+    new Promise((resolve, reject) => {
+      middleware(req, res, (result) => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+        return resolve(result);
+      });
+    });
+}
+
+const cors = initMiddleware(
+  Cors({
+    methods: ["GET", "OPTIONS"],
+    origin: true, // TODO: Might want to make this work for a set, like just observable but it's fine for now...
+  })
+);
+
+export default async (req, res) => {
+  try {
+    await cors(req, res);
+
+    return axios
+      .get(req.query.dest, {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36",
+        },
+      })
+      .then((r) => {
+        res.status(200).send(r.data);
+      })
+      .catch((e) => {
+        console.error(e);
+        res.status(500).send(`Something went wrong: ${e}`);
+      });
+  } catch (e) {
+    console.error(e);
+    res.status(500).send(`Something went wrong: ${e}`);
+  }
+};
